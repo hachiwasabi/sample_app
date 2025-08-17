c89dcf54c9b611e573517f0cf247a05141351236
hachiwasabi <224736665+hachiwasabi@users.noreply.github.com>
2025-08-17 20:34:44 +0000

    [Finish]アプリ7

--- config/routes.rb
+++ config/routes.rb
@@ -3,10 +3,12 @@ Rails.application.routes.draw do
   #get "lists/edit"
   get 'lists/:id/edit' => 'lists#edit', as: 'edit_list'
   get '/top' => 'homes#top'
+  resources :lists
   post 'lists' => 'lists#create'
   get 'lists' => 'lists#index'
   get 'lists/:id' => 'lists#show', as: 'list'
   patch 'lists/:id' => 'lists#update', as: 'update_list'
+  delete 'lists/:id' => 'lists#destroy', as: 'destroy_list'
   #get "lists/index"
   #get "lists/show"  
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
